a2ddada2f95d2058f96c8baf51f786c64fdcb239
Cathy Hui <erashine414@icloud.com>
2023-06-09 18:39:41 +0800

    revisited the concepts of how spread operator works for iterables, ie array, string, maps,sets and object

---
+++ Notebooks /Advanced Javascript - Jonas Full Course/Data Structure /09-Revisted Spread Operator /script.js
@@ -0,0 +1,192 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: [
+    'Focaccia',
+    'Bruschetta',
+    'Garlic Bread',
+    'Caprese Salad',
+    ['Fried rice, Ice cream'],
+  ],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex, categoriesIndex) {
+    return [
+      this.starterMenu[starterIndex],
+      this.mainMenu[mainIndex],
+      this.categories[categoriesIndex],
+    ];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 0,
+    mainIndex = 0,
+    time = '19:30',
+    address,
+  }) {
+    console.log(
+      `Order already received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Hey here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+};
+
+restaurant.orderDelivery({
+  starterIndex: 2,
+  mainIndex: 1,
+  time: '23:00',
+  address: 'Demot Vol 45',
+});
+
+restaurant.orderDelivery({
+  mainIndex: 1,
+  address: 'Demot Vol 45',
+});
+
+// Real-world example
+const ingredients = [
+  // prompt('Ingredient 1?'),
+  // prompt('Ingredient 2?'),
+  // prompt('Ingredient 3?'),
+];
+
+console.log(ingredients);
+
+restaurant.orderPasta(...ingredients);
+
+// Topic C - Spread Operator: whenever we need the individual elements of an array, the individual values/elements can be passed into function as arguements
+const arrOld = [9, 5, 0, 3];
+const goodArr = [1, 2, ...arrOld];
+console.log(goodArr);
+console.log(...goodArr);
+
+const newMenu = ['Apple Pie', ...restaurant.mainMenu];
+console.log(newMenu);
+
+function add1(a, b, c, d) {
+  return a + b + c + d;
+}
+
+console.log(add1(...arrOld));
+
+// 2. Array Copy - Shallow copy
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+// 3. Merge arrarys - only merge the top level of array
+const mergeMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(mergeMenu);
+
+// 4. Spread Operator can be used on Iterables - array, string, maps, sets. Or object
+
+const str = 'Cathy';
+console.log(...str);
+const newStr = [...str, '', '!!!!'];
+console.log(newStr);
+
+// 5. Spread Operator for object
+
+const newEatingPlace = {
+  ...restaurant,
+  founder: 'Catcatmiu',
+  Yearfounded: '2023',
+};
+console.log(newEatingPlace);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'abc';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+/*
+// Topic A - Array Destructuring
+
+// 1. Array Destructuring Assignment Syntax: unpacking the original array & would not change the original array
+const arr = [2, 3, 4];
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// 2. Array Destructuring: leave a hole like below will skip one element of the original array
+let [main, , , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// 3. Array Destructuring: switching variables
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// 4. Receive multiple return values from a function
+const [starter, mainDish, categoriesDish] = restaurant.order(2, 0, 2);
+console.log(starter, mainDish, categoriesDish);
+
+// 5. Destructuring Nested Array
+const nested = [2, 4, [5, 7, 9]];
+const [i, , j] = nested;
+console.log(i, j);
+const [l, , [k, m, n]] = nested;
+console.log(l, k, m, n);
+
+// 6. Default values: set default value to the left side of the destructing and when certain positions of the arrary of the right side do not exist, the right side would return the default value
+const defaultArr = [99, 44];
+const [p = 0, q = 0, r = 1, d = 1] = defaultArr;
+console.log(p, q, r, d);
+
+// Topic B: Object Destructruing
+// 1. Object Destructruing - Basic Syntax
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// 2. How to assign new variable names to properties destructured
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: type,
+} = restaurant;
+
+console.log(restaurantName, hours, type);
+
+// 3. Set default value to property that does not exist in the object
+const { menu = [], starterMenu: starters, mainMenu: mains } = restaurant;
+console.log(menu, starters, mains);
+
+// 4. Mutating variables
+let a = 444;
+let b = 999;
+const obj1 = { a: 14, b: 99, c: 7 };
+({ a, b } = obj1); // Mutated the a, b varialbes declared with the value of a & b from obj1
+console.log(a, b);
+
+// 5. Nested objects - How to retrive & mutate variable name
+
+const {
+  sat: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+// 6. Small application ultilizing all the features above for object destructuring - code lines 39 - 60. It basically takes obj and pass into a function as an argrument and destructure the obj in the function immediately. This method is extermely useful for passing multiple arguements into a function without defining it.
+*/
